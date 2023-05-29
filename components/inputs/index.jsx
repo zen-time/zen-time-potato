57f65552a8b8e6cd59d8eb353efb4d74d08548c6
vishnu <vishnu@mericaninc.com>
2023-05-29 15:00:12 +0530

    create three password input field

--- components/inputs/index.jsx
+++ components/inputs/index.jsx
@@ -1,11 +1,44 @@
 const Inputs = () => {
   return (
-    <>
+    <div className="w-full bg-inherit mx-auto my-0 mt-10 ">
       {" "}
-      <input type="password" />
-      <input type="radio" />
-      <input type="month" />
-    </>
+      <div className=" w-1/4 mx-auto my-0 flex flex-col justify-center gap-5 bg-gray-100 p-5 shadow-lg shadow-gray-500/40">
+        <input
+          type="password"
+          placeholder="Password"
+          className=" p-2  pl-4 rounded-lg bg-gray cursor-pointer border-solid outline-white"
+        />
+        <div>
+          <label className="flex my-1 text-slate-400">Password</label>
+          <input
+            type="password"
+            className="p-2 w-3/4 bg-transparent border-2 border-gray-200 outline-gray-300 cursor-pointer"
+          />
+        </div>
+        <div className="my-2">
+          <input
+            type="password"
+            placeholder="Password"
+            className=" p-2 w-60 pl-4 rounded-full  cursor-pointer shadow-inner  shadow-zinc-300 outline-none"
+          />
+        </div>
+
+        <hr />
+        <div>
+          <input
+            type="radio"
+            className=" border-slate-300 outline-none  cursor-pointer hover:outline-blue-400"
+          />
+          <input
+            type="radio"
+            className=" border-slate-300 outline-none w-12 cursor-pointer rounded-full"
+          />
+        </div>
+
+        <hr />
+        <input type="month" className="border border-slate-300" />
+      </div>
+    </div>
   );
 };
 export default Inputs;

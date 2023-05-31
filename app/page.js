57108550af9228edde33c449bbde87e77c3505af
kesavan <kesavan@mericaninc.com>
2023-05-31 12:14:29 +0530

    add reusableinputcomponent

--- app/page.js
+++ app/page.js
@@ -1,9 +1,25 @@
-import Inputs from "@components/inputs";
+// import Inputs from "@components/inputs";
+
+import ReusableInputComponent from "@components/inputs/reusableinputcomponent";
 
 const Home = () => {
   return (
     <>
-      <Inputs />
+      {/* <Inputs /> */}
+      <ReusableInputComponent
+        type="range"
+        min="0"
+        max="1000"
+        step="50"
+        list="marker"
+        label="range input"
+        id="range"
+      />
+      <ReusableInputComponent
+        type="search"
+        placeholder="search..."
+        id="search"
+      />
     </>
   );
 };
--- components/inputs/index.jsx
+++ components/inputs/index.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+// import React from "react";
 
-const index = () => {
-  return <div>index</div>;
+// const index = () => {
+//   return <div>index</div>;
 
-import Inputs from "./Inputs";
-const index = () => {
-  return (
-    <div>
-      <Inputs />
-    </div>
-  );
-};
+// import Inputs from "./Inputs";
+// const index = () => {
+//   return (
+//     <div>
+//       <Inputs />
+//     </div>
+//   );
+// };
 
-export default index;
+// export default index;

8f5c3c1e3c3b02ff679896cd533bf4bbdabb4ebc
dheena <dheena@mericaninc.com>
2023-06-02 11:10:44 +0530

    new table checkbpox create

--- components/tables/selectedtable.jsx
+++ components/tables/selectedtable.jsx
@@ -2,12 +2,26 @@ import React from 'react'
 import { Company } from './selectedtabledata';
 
 const Selectedtable = () => {
+
+  // const [user,setUser] = useState([]);
+
+
+  // useEffect(()=>{
+  //   setUser(Company);
+  // },[])
+
+
+
+
   return (
-    <div>
-      <table>
-        <thead>
+    <div className='flex justify-center items-center flex-col gap-1'>
+      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400" >
+      <thead className='text-xl text-center  rounded-xl text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400'>
             <tr>
-                <th>#</th>
+              <th>
+                <input type="checkbox" />
+              </th>
+                <th className='p-3 text-center'>#</th>
                 <th>First Name</th>
                 <th>Last Name</th>
                 <th>Email</th>
@@ -15,22 +29,26 @@ const Selectedtable = () => {
                 <th>Company Name</th>
             </tr>
         </thead>
-        <tbody>
+        <tbody className='border-2'>
             {Company.map((item)=>{
                 return(
-                    <tr>
-                        <td>{item.id}</td>
-                        <td>{item.first_name}</td>
-                        <td>{item.last_name}</td>
-                        <td>{item.email}</td>
-                        <td>{item.Role}</td>
-                        <td>{item['Company Name']}</td>
+                    <tr className='text-center hover:bg-lime-100'>
+                      <td>
+                        <input type="checkbox" className='p-2 hover:text-black text-center border-2 border-x-2' />
+                      </td>
+                        <td className='p-2 hover:text-black text-center border-2 border-x-2'>{item.id}</td>
+                        <td className='text-center p-3   hover:text-black border-2 border-x-2'>{item.first_name}</td>
+                        <td className='text-center p-2 hover:text-black border-2 border-x-2'>{item.last_name}</td>
+                        <td className='text-center p-2 hover:text-black border-2 border-x-2'>{item.email}</td>
+                        <td className='text-center p-2 hover:text-black border-2 border-x-2'>{item.Role}</td>
+                        <td className='text-center p-2 hover:text-black border-2 border-x-2'>{item['Company Name']}</td>
 
                     </tr>
                 )
             })}
         </tbody>
       </table>
+      {/* <hr /> */}
     </div>
   )
 }

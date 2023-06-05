5e099a869ca3ca064bf0033292b82e4da6452587
akashsrma <akash@mericaninc.com>
2023-06-05 16:47:13 +0530

    added btn new

--- app/page.js
+++ app/page.js
@@ -1,7 +1,62 @@
-import Button from "@components/buttons/buttons";
+
+import React from "react";
+import "@styles/globals.css";
+import Buttondesignone from "@components/buttons/buttondesignone";
 
 const Home = () => {
-  return <div><Button/></div>;
+
+return(
+  <>
+   {/* Normal Effect */}
+
+  <div className="flex items-center justify-center  gap-[21rem] my-[3rem]">
+    <Buttondesignone label={"Button Hover"} type={"btn_red"} />
+    <Buttondesignone label={"Button Hover"} type={"btn_green"} />
+    <Buttondesignone label={"Button Hover"} type={"btn_gray"} />
+
+    </div>
+
+ {/* Right to Left Hover Effect */}
+    
+    <div className="flex items-center justify-center gap-[21rem] mx-[4rem] my-[6rem]">
+          <Buttondesignone label={"Button Hover"} type={"btn_redright"} />
+          <Buttondesignone label={"Button Hover"} type={"btn_greenright"} />
+          <Buttondesignone label={"Button Hover"} type={"btn_grayright"} />
+           
+        </div>
+
+       {/* Left  to Right Hover Effect */}
+
+      <div className="flex items-center justify-center gap-[21rem] mx-[4rem] my-[6rem]">
+        <Buttondesignone label={"Button Hover"} type={"btn_redhover"} />
+        <Buttondesignone label={"Button Hover"} type={"btn_greenhover"}  />
+        <Buttondesignone label={"Button Hover"} type={"btn_grayhover"} />
+        
+        </div>    
+
+      {/* Ring hover effect */}
+      
+        <div className="flex items-center justify-center gap-[21rem] mx-[4rem] my-[6rem]">
+        <Buttondesignone label={"Button Hover"} type={"btn_redring"} />
+        <Buttondesignone label={"Button Hover"} type={"btn_greenring"}  />
+        <Buttondesignone label={"Button Hover"} type={"btn_grayring"} />
+        
+        </div> 
+
+
+
+      
+
+  
+ 
+  
+
+
+ 
+  
+  </>
+)
+
 };
 
 export default Home;

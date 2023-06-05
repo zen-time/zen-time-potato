6e434b3bfe9207f75a702fcc416b856dfcf72265
akashsrma <akash@mericaninc.com>
2023-06-05 16:46:37 +0530

    added button new file

--- components/buttons/buttons.jsx
+++ components/buttons/buttons.jsx
@@ -1116,6 +1116,66 @@ const Button = () => {
   
   
   
+  <div  className='flex gap-60'>
+    <div>
+    <button className="relative inline-flex items-center justify-center  px-6 py-2 overflow-hidden font-medium  transition duration-300 ease-out border-2 border-orange-500 rounded-full shadow-md group">
+<span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-orange-500 group-hover:translate-x-0 ease">
+<svg className="w-6 h-6" fill="none" stroke="currentColor" ><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+</span>
+<span className="absolute flex items-center justify-center w-full h-full text-orange-500 transition-all duration-300 transform group-hover:translate-x-full ease">Button Text</span>
+<span className="relative invisible">Button Text</span>
+</button>
+    </div>
+    <div>
+    <button className="relative inline-flex items-center justify-center  px-6 py-2 overflow-hidden font-medium  transition duration-300 ease-out border-2 border-pink-500 rounded-full shadow-md group">
+<span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-pink-500 group-hover:translate-x-0 ease">
+<svg className="w-6 h-6" fill="none" stroke="currentColor" ><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+</span>
+<span className="absolute flex items-center justify-center w-full h-full text-pink-500 transition-all duration-300 transform group-hover:translate-x-full ease">Button Text</span>
+<span className="relative invisible">Button Text</span>
+</button>
+    </div>
+    <div>
+    <button className="relative inline-flex items-center justify-center  px-6 py-2 overflow-hidden font-medium  transition duration-300 ease-out border-2 border-red-500 rounded-full shadow-md group">
+<span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-red-500 group-hover:translate-x-0 ease">
+<svg className="w-6 h-6" fill="none" stroke="currentColor" ><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+</span>
+<span className="absolute flex items-center justify-center w-full h-full text-red-500 transition-all duration-300 transform group-hover:translate-x-full ease">Button Text</span>
+<span className="relative invisible">Button Text</span>
+</button>
+    </div>
+  </div>
+
+
+  <div  className='flex gap-60'>
+    <div>
+    <button className="relative inline-flex items-center justify-center  px-6 py-2 overflow-hidden font-medium  transition duration-300 ease-out border-2 border-gray-800 rounded-full shadow-md group">
+<span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-gray-800 group-hover:translate-x-0 ease">
+<svg className="w-6 h-6" fill="none" stroke="currentColor" ><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+</span>
+<span className="absolute flex items-center justify-center w-full h-full text-gray-800 transition-all duration-300 transform group-hover:translate-x-full ease">Button Text</span>
+<span className="relative invisible">Button Text</span>
+</button>
+    </div>
+    <div>
+    <button className="relative inline-flex items-center justify-center  px-6 py-2 overflow-hidden font-medium  transition duration-300 ease-out border-2 border-red-800 rounded-full shadow-md group">
+<span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-red-800 group-hover:translate-x-0 ease">
+<svg className="w-6 h-6" fill="none" stroke="currentColor" ><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+</span>
+<span className="absolute flex items-center justify-center w-full h-full text-red-800 transition-all duration-300 transform group-hover:translate-x-full ease">Button Text</span>
+<span className="relative invisible">Button Text</span>
+</button>
+    </div>
+    <div>
+    <button className="relative inline-flex items-center justify-center  px-6 py-2 overflow-hidden font-medium  transition duration-300 ease-out border-2 border-green-500 rounded-full shadow-md group">
+<span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-green-500 group-hover:translate-x-0 ease">
+<svg className="w-6 h-6" fill="none" stroke="currentColor" ><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+</span>
+<span className="absolute flex items-center justify-center w-full h-full text-green-500 transition-all duration-300 transform group-hover:translate-x-full ease">Button Text</span>
+<span className="relative invisible">Button Text</span>
+</button>
+    </div>
+  </div>
   
   
   

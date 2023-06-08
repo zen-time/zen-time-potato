abc977df6c834c9cf41c3dd8cf2dc04c3c1e7376
vishnu <vishnu@mericaninc.com>
2023-06-08 13:00:14 +0530

    create input fields

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
+import AnotherTable from "@components/tables/anotherTable";
 import ClientTable from "@components/tables/clientTable";
 import React from "react";
 
 const Home = () => {
   return (
     <div>
-      <ClientTable />
+      <ClientTable textColor={"table_head_text"} titleColor={"title"} />
+      {/* <AnotherTable /> */}
     </div>
   );
 };
--- components/tables/clientTable.jsx
+++ components/tables/clientTable.jsx
@@ -1,32 +1,41 @@
 import React from "react";
 
-const ClientTable = () => {
+const ClientTable = ({ textColor, titleColor }) => {
   return (
     <div className="m-10">
+      <h2 className={`my-10 ${titleColor}`}>Client Table</h2>
+      {/* input fields */}
+      <div className="flex justify-around">
+        <input className="input  input-ghost border-gray-300 " />
+
+        <button>Clear</button>
+      </div>
       <div className="overflow-x-auto">
-        <table className="table">
+        <table className="table table-md border-2 border-gray-200 ">
           {/* head */}
-          <thead>
-            <tr className="bg-neutral-content">
+          <thead className="">
+            <tr className={`bg-neutral-content text-base ${textColor}`}>
               <th></th>
-              <th>Business Name</th>
-              <th>Client ID</th>
-              <th>Email</th>
-              <th>Contact Number</th>
-              <th>Net Terms</th>
-              <th>Termination Notice</th>
-              <th>Status</th>
-              <th>Category</th>
-              <th>Additional Info</th>
-              <th>Active Placements</th>
-              <th>Action</th>
+              <th className=" text-gray-400  ">Business Name</th>
+              <th className=" text-gray-400  ">Client ID</th>
+              <th className=" text-gray-400  ">Email</th>
+              <th className=" text-gray-400  ">Contact Number</th>
+              <th className=" text-gray-400  ">Net Terms</th>
+              <th className=" text-gray-400  ">Termination Notice</th>
+              <th className=" text-gray-400  ">Status</th>
+              <th className=" text-gray-400  ">Category</th>
+              <th className=" text-gray-400  ">Additional Info</th>
+              <th className=" text-gray-400  ">Active Placements</th>
+              <th className=" text-gray-400  ">Action</th>
             </tr>
           </thead>
-          <tbody>
+          <tbody className="divide-x-10 divide-green-500">
             {/* row 1 */}
-            <tr className="hover">
+            <tr className="hover ">
               <th>1</th>
-              <td>Akash </td>
+              <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap">
+                Akash{" "}
+              </td>
               <td>75123</td>
               <td>akash@inc.com</td>
               <td>9629653377</td>
@@ -41,7 +50,9 @@ const ClientTable = () => {
             {/* row 2 */}
             <tr className="hover">
               <th>2</th>
-              <td>Kesavan</td>
+              <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap">
+                Kesavan
+              </td>
               <td>75124</td>
               <td>kesavan@inc.com</td>
               <td>8075423337</td>
@@ -56,8 +67,10 @@ const ClientTable = () => {
             {/* row 3 */}
             <tr className="hover">
               <th>3</th>
-              <td>Naveen Kumar</td>
-              <td>75124</td>
+              <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap">
+                Naveen Kumar
+              </td>
+              <td>75125</td>
               <td>naveen@inc.com</td>
               <td>902965723</td>
               <td>-</td>
@@ -71,7 +84,9 @@ const ClientTable = () => {
             {/* row 4 */}
             <tr className="hover">
               <th>4</th>
-              <td>Arun Prakash</td>
+              <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap">
+                Arun Prakash
+              </td>
               <td>75126</td>
               <td>arun@inc.com</td>
               <td>9945687219</td>
@@ -86,8 +101,10 @@ const ClientTable = () => {
             {/* row 5 */}
             <tr className="hover">
               <th>5</th>
-              <td>Manoj</td>
-              <td>75126</td>
+              <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap">
+                Manoj
+              </td>
+              <td>75127</td>
               <td>manoj@inc.com</td>
               <td>9956231245</td>
               <td>-</td>
@@ -101,8 +118,10 @@ const ClientTable = () => {
             {/* row 6 */}
             <tr className="hover">
               <th>6</th>
-              <td>Vishnu</td>
-              <td>75123</td>
+              <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap">
+                Vishnu
+              </td>
+              <td>75128</td>
               <td>vishnu@inc.com</td>
               <td>9366144319</td>
               <td>-</td>
